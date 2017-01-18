83dfa6526af232cc72ff7255cf72fd5f2ba3f953
Tonya Mork <hellofromtonya@uptechlabs.io>
2017-01-18 16:32:18 -0600

    Let WordPress load Genesis

--- functions.php
+++ functions.php
@@ -13,6 +13,3 @@ namespace KnowTheCode\Developers;
 include_once( 'lib/init.php' );
 
 include_once( 'lib/functions/autoload.php' );
-
-// Start the Genesis framework.
-include_once( get_template_directory() . '/lib/init.php' );
