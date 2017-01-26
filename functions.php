1a5d5d6969530855bb7ac3724d00c76997d2c754
Tonya Mork <hellofromtonya@uptechlabs.io>
2017-01-26 12:23:19 -0600

    added teaching moment
    We don't need to load Genesis framework from our child theme.  I added a teaching moment to explain why.

--- functions.php
+++ functions.php
@@ -13,3 +13,19 @@ namespace KnowTheCode\Developers;
 include_once( 'lib/init.php' );
 
 include_once( 'lib/functions/autoload.php' );
+
+/**
+ * This line of code is NOT needed.  It's redundant. Why?
+ *
+ * WordPress loads the child theme's functions.php file first.  Then it loads 
+ * the parent theme.  Think about that.  Genesis' functions.php is going to load
+ * up right after this line of code.
+ *
+ * Open up `genesis/functions.php`.  What's the only line of code in there?
+ *
+ * Right - start the engine.  It loads the `lib/init.php` itself. 
+ *
+ * Therefore, this line of code is not needed.  Our architecture in this child theme
+ * loads the child theme FIRST before the Genesis framework loads.
+ */
+// include_once( get_template_directory() . '/lib/init.php' );
